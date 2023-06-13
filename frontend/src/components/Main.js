901ca375d209dbf1d15f93314f0f44b30e13e0da
ilfm <dnehd1828@gmail.com>
2023-06-13 14:17:30 +0900

    no message

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router";
 import SampleHome from "./SampleHome";
+import Community from "../community/Community";
 
 const Main = () => {
   return (
@@ -7,7 +8,7 @@ const Main = () => {
       <Routes>
         <Route path='/' element={<SampleHome />} />
         <Route path='/search' element={<SampleHome />} />
-        <Route path='/community' element={<SampleHome />} />
+        <Route path='/community' element={<Community />} />
         <Route path='/my-plants' element={<SampleHome />} />
         <Route path='/growth-journal' element={<SampleHome />} />
       </Routes>
